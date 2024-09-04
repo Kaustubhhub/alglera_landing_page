3095183101f41ba3f77ccd5c21a917308ff0793b
Kaustubh <kaustubhkumbhare02@gmail.com>
2024-09-04 12:32:49 +0530

    added beams  background

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import { BackgroundBeams } from "@/components/ui/background-beams";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <BackgroundBeams/>
     </main>
   );
 }
